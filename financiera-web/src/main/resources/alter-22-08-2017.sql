7af4e6c009931d5529a2c94317022a543c890ee4
Lucas <ldifabio@unrn.edu.ar>
2017-08-22 18:09:41 -0300

    Modelado y mapeo de Credito-Interes-Estado.

---
+++ financiera-web/src/main/resources/alter-22-08-2017.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `financiera_dev`.`interes` (
+  `id`    BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `valor` DOUBLE     NOT NULL,
+  `orden` INT        NOT NULL,
+  PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB;
+CREATE TABLE `financiera_dev`.`credito` (
+  `id`                BIGINT(20)   NOT NULL AUTO_INCREMENT,
+  `capital`           DOUBLE       NOT NULL,
+  `cuotas`            INT          NOT NULL,
+  `fecha`             DATE         NOT NULL,
+  `fecha_vencimiento` DATE         NOT NULL,
+  `estado`            VARCHAR(255) NOT NULL,
+  `interes_id`        BIGINT(20)   NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX (`interes_id`)
+)
+  ENGINE = InnoDB;
+
+ALTER TABLE `credito` ADD FOREIGN KEY (`interes_id`) REFERENCES `interes`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
