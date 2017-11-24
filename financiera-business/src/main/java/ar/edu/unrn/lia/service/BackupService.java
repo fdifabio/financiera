8eb90d4bb0d213819c3b5a5b4a6569f3891f9372
fdifabio <fdifabio92@gmail.com>
2017-11-24 11:21:45 -0300

    Backup

--- financiera-business/src/main/java/ar/edu/unrn/lia/service/BackupService.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unrn.lia.service;
-
-/**
- * Created by Federico on 23/11/2017.
- */
-public interface BackupService {
-
-    void backup();
-}
---
+++ financiera-web/src/main/java/ar/edu/unrn/lia/bean/util/Backup.java
@@ -0,0 +1,39 @@
+package ar.edu.unrn.lia.bean.util;
+
+import ar.edu.unrn.lia.config.ParamValue;
+
+/**
+ * Created by Federico on 24/11/2017.
+ */
+public class Backup {
+
+        @ParamValue(key = "backup.pathSQL")
+        private static String pathSQL;
+
+        @ParamValue(key = "backup.command")
+        private static String command;
+
+        @ParamValue(key = "backup.pathBack")
+        private static String pathBack;
+
+        public void backup() {
+            Process p = null;
+            try {
+                Runtime runtime = Runtime.getRuntime();
+                p = runtime.exec(pathSQL + command + pathBack);
+//change the dbpass and dbname with your dbpass and dbname
+                int processComplete = p.waitFor();
+
+                if (processComplete == 0) {
+                    System.out.println("Backup created successfully!");
+                } else {
+                    System.out.println("Could not create the backup");
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
